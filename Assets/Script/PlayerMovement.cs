606649353361c8d16fb38800bdb42ae215f7768c
An <162073486+AnDayno@users.noreply.github.com>
2025-07-30 07:07:58 +0800

    Added movement left and right

--- Assets/Script/PlayerMovement.cs
+++ Assets/Script/PlayerMovement.cs
@@ -3,23 +3,32 @@ using System.Collections.Generic;
 using UnityEngine.InputSystem;
 using UnityEngine;
 using Unity.VisualScripting;
+using System;
 
 public class PlayerMovement : MonoBehaviour
 {
     Vector2 moveInput;
-    void Start()
+    Rigidbody2D rb2d;
+
+    [SerializeField] float playerSpeed = 5f;
+    private void Awake()
     {
-        
+        rb2d = GetComponent<Rigidbody2D>();
     }
 
     void Update()
     {
-         
+        Run();
     }
 
     void OnMove(InputValue value)
     {
         moveInput = value.Get<Vector2>();
         Debug.Log("Move Input: " + moveInput);
+    }
+    void Run()
+    {
+        Vector2 playerVelocity = new Vector2(moveInput.x * playerSpeed, rb2d.velocity.y);
+        rb2d.velocity = playerVelocity;
     }
 }
