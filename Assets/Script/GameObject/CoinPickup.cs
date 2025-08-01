bc895c58dc60b0e6bef8e1a07edcd25c2b0255d3
An <162073486+AnDayno@users.noreply.github.com>
2025-08-01 16:33:36 +0800

    Added sound on coin pickup

--- Assets/Script/GameObject/CoinPickup.cs
+++ Assets/Script/GameObject/CoinPickup.cs
@@ -4,11 +4,14 @@ using UnityEngine;
 
 public class CoinPickup : MonoBehaviour
 {
+    [SerializeField] AudioClip coinPickup;
+
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.CompareTag("Player"))
         {
             Destroy(gameObject);
+            AudioSource.PlayClipAtPoint(coinPickup, Camera.main.transform.position);
         }
     }
 }
